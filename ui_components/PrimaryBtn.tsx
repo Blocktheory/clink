5b75ef744bcf5e91634dcc8bc83f4a45d41ccab6
shreyank4444 <shreyank@designstring.com>
2023-08-03 16:06:58 +0530

    share link page update

--- ui_components/PrimaryBtn.tsx
+++ ui_components/PrimaryBtn.tsx
@@ -7,11 +7,12 @@ interface IPrimaryBtn {
     title: string;
     onClick: React.MouseEventHandler<HTMLButtonElement>;
     leftImage?: Record<TImages, TNextImage>;
-    rightImage?: TNextImage | TImages;
+    rightImage?: TNextImage | TImages | string;
+    showShareIcon?: boolean;
 }
 
 export default function PrimaryBtn(props: IPrimaryBtn) {
-    const { title, onClick, rightImage } = props;
+    const { title, onClick, rightImage, showShareIcon } = props;
     return (
         <button
             className="py-4 btnBg support_text_bold rounded-lg flex gap-1 items-center w-[90%] md:w-[60%] lg:w-[320px] justify-center my-0 mx-auto max-w-[320px] relative z-10"
